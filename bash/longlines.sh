7be1f3f3e55ce21f5d75a4776859eaf07c8f7770
Matthew <matthew.aquino1998@gmail.com>
2019-01-22 13:24:09 -0800

    commit

--- bash/longlines.sh
+++ bash/longlines.sh
@@ -9,18 +9,16 @@ The string is created on the command line, like and other string.
 "
 
 # multiline pipeline sequence
-echo "This is an example of a command that spans lines by continuing a pipeline sequence" |
-    sed "aThe additional lines are indented for code clarity" |
-    tac # the tac reverses the order of the output lines
+echo "The additional lines are indented for code clarity" |
+    sed "aThis is an example of a command that spans lines by continuing a pipeline sequence" |
 
 # multiline command using backlash to escape the end of line character
 echo \
     "This line continues because we turned off the end of line character on the first line"
-    
 
 # multiline command due to inline data using HERE document
-cat <<EOF
+cat <<EndOfFile
 This is a sample of multiline text data using a HERE document.
 You can indent this one if you wish, by disabling input tabs,
 but you still might not get what you want.
-EOF
+EndOfFile
